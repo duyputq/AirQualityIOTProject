d87dea583de44244d4ae8e967d34893bb862d7f2
Duy <duy20nguyen04@gmail.com>
2024-11-24 21:39:59 +0700

    update blynk

--- AirQualityPrj.ino
+++ AirQualityPrj.ino
@@ -1,8 +1,18 @@
-#include <DHT.h>
-
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPL6BX-eeWqf"
+#define BLYNK_TEMPLATE_NAME "powder"
+#define BLYNK_AUTH_TOKEN "MqwtlkSnRYULJIC1FCNpPR8hVqtRvSgJ"
 //dht
 #define DHTPIN 4 //(D2)
 #define DHTTYPE DHT11 // Dinh nghia loai cam bien DHT
+
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#include <WiFiClient.h> 
+#include <BlynkSimpleEsp8266.h>
+
+char ssid[] = "smarthome";
+char pass[] = "smarthome";
 
 //gas sensor
 int Gas_analog = A0;
@@ -13,6 +23,7 @@ const int bb = 23;
 const int pwmMotorA = D1;
 const int dirMotorA = D3;
 int motorSpeed = 100;
+int button;
 
 DHT dht(DHTPIN, DHTTYPE); //Set up cam bien nhiet do
 
@@ -24,10 +35,25 @@ void setup() {
   pinMode(Gas_digital, INPUT);
   pinMode(pwmMotorA, OUTPUT);
   pinMode(dirMotorA, OUTPUT);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
   delay(1000);
 }
 
+BLYNK_WRITE (V1)
+{
+  button = param.asInt();
+  if (button ==1) {
+    digitalWrite(pwmMotorA, motorSpeed);
+    digitalWrite(dirMotorA, LOW);
+  }
+  else {
+    digitalWrite(pwmMotorA, 0);
+    digitalWrite(dirMotorA, LOW);
+  }
+}
+
 void loop() {
+  Blynk.run();
   float h = dht.readHumidity();    // Doc do am tu cam bien
   float t = dht.readTemperature(); // Doc nhiet do tu cam bien
 
@@ -48,13 +74,17 @@ void loop() {
   Serial.print(gassensorAnalog);
   Serial.println();
 
-  Serial.println("Kich hoat motor A quay trong 5s");
-  digitalWrite(pwmMotorA, motorSpeed);
-  digitalWrite(dirMotorA, LOW);
-  delay(5000);
+  Blynk.virtualWrite(V2, t);
+  Blynk.virtualWrite(V3, h);
+  Blynk.virtualWrite(V0, gassensorAnalog);
 
-  Serial.println("Stop motor A trong 2s ");
-  digitalWrite(pwmMotorA, 0);
-  digitalWrite(dirMotorA, LOW);
-  delay(2000);
+  // Serial.println("Kich hoat motor A quay trong 5s");
+  // digitalWrite(pwmMotorA, motorSpeed);
+  // digitalWrite(dirMotorA, LOW);
+  // delay(5000);
+
+  // Serial.println("Stop motor A trong 2s ");
+  // digitalWrite(pwmMotorA, 0);
+  // digitalWrite(dirMotorA, LOW);
+  delay(1000);
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ![devices](https://github.com/user-attachments/assets/eb823e1a-11c9-4831-a8c4-945aa29b8768)
 
 ![stastics](https://github.com/user-attachments/assets/8508a6cf-2278-43a5-a2fc-8f08c5cc8efa)
+
+cảm biến sử dụng
+- dht 11
+- mq2
